97841c5df4797c541700bb9051b0d007e241580f
Maria <marfayaga@gmail.com>
2023-08-24 04:17:03 +0300

    add socket support

--- server/index.mjs
+++ server/index.mjs
@@ -2,19 +2,46 @@ import fastify from 'fastify'
 import fastifyStatic from '@fastify/static'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import { WebSocketServer } from 'ws'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const server = fastify();
 
+const wss = new WebSocketServer({ server: server.server })
+
+wss.on('connection', client => {
+  console.log('----NEW CLIENT CONNECTED')
+
+  client.on('message', data => {
+    console.log('GOT MESSAGE', data.toString())
+    wss.clients.forEach(otherClient => {
+      if (otherClient !== client) {
+        otherClient.send('Message from another client: ' + data) 
+      }
+    })
+  })
+
+  client.on('close', () => {
+    console.log('----CLIENT DISCONNECTED')
+  })
+})
+
+server.setNotFoundHandler((_, reply) => {
+  return reply.sendFile('index.html')
+})
+
 server.register(fastifyStatic, {
   root: path.join(__dirname, '../dist')
 })
 
-server.listen({ port: 5678 })
-  .then(() => {
-    console.log('Server started')
+const port = process.env.PORT || 5679;
+const host = process.env.HOST || 'localhost'
+
+server.listen({ port, host })
+  .then((address) => {
+    console.log('Server started: ' + address)
   })
   .catch((error) => {
     console.log('Error', error)
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import './app.scss'
 
 export default () => {
-  return <h1>Hello world!</h1>
+  const [isConnected, setIsConnected] = useState(false)
+  useEffect(() => {
+    const ws = new WebSocket(`ws://${location.host}`)
+    ws.onopen = () => {
+      console.log('Connected')
+      ws.send('Ololo')
+      setIsConnected(true)
+    }
+
+    ws.onmessage = e => {
+      console.log(e.data)
+    }
+
+    ws.onclose = () => {
+      console.log('Disconnected')
+      setIsConnected(false)
+    }
+  }, [])
+
+  return <div>
+    <h1>Hello world!</h1>
+    <p>{isConnected ? 'Connected' : 'Disconnected'}</p>
+  </div>
 }
