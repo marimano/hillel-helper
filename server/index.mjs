e42c1372529493e23689c2a4105471896473b271
Maria <marfayaga@gmail.com>
2023-09-04 20:02:10 +0300

    requested changes: remove copy-paste

--- server/index.mjs
+++ server/index.mjs
@@ -17,12 +17,8 @@ wss.on('connection', client => {
   client.on('message', data => {
     console.log('GOT MESSAGE', data.toString())
     wss.clients.forEach(otherClient => {
-      if (otherClient !== client) {
-        otherClient.send('Message from another client: ' + data) 
-      }
-      else {
-        otherClient.send('Message from you: ' + data) 
-      }
+      const sender = otherClient === client ? 'you' : 'another client'
+      otherClient.send(`Message from ${sender}: ` + data) 
     })
   })
 
